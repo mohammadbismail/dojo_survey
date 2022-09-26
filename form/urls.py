f78dc06888f19650307ea7aa0ddb34df43859741
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-26 15:58:17 +0300

    final_sub

--- form/urls.py
+++ form/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("input", views.create_users),
     path("result", views.result),
 ]
--- form/views.py
+++ form/views.py
@@ -5,21 +5,20 @@ def index(request):
     return render(request, "index.html")
 
 
-def create_users(request):
+def result(request):
     name_from_form = request.POST["name"]
     location_from_form = request.POST["location"]
     language_from_form = request.POST["language"]
     comment_from_form = request.POST["comment"]
-    print(request.POST)
     context = {
         "name_on_template": name_from_form,
         "dojo_location": location_from_form,
-        "Favorite_language": language_from_form,
+        "favorite_language": language_from_form,
         "comment": comment_from_form,
     }
-    return redirect("/result")
+    return render(request, "result.html", context)
 
 
-def result(request):
+# def result(request):
 
-    return render(request, "result.html")
+#     return render(request, "result.html")
